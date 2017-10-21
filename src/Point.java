713f170d39204359a337e1f1738a7c2c1231ebe3
HJS <407410014@qq.com>
2017-10-21 11:16:09 +0800

    finish Point.java

--- src/Point.java
+++ src/Point.java
@@ -60,7 +60,19 @@ public class Point implements Comparable<Point> {
      */
     public double slopeTo(Point that) {
         /* YOUR CODE HERE */
-        return -1;
+        if (that == null)    throw new NullPointerException();
+
+        double slope;
+        int dX = that.x - x;     // distance of direction X
+        int dY = that.y - y;     // distance of direction Y
+
+        if (dX == 0)
+        {
+            if (dY == 0)     return Double.NEGATIVE_INFINITY;
+            return Double.POSITIVE_INFINITY;
+        }
+        slope = dY / dX;
+        return slope + 0.0;
     }
 
     /**
@@ -77,7 +89,15 @@ public class Point implements Comparable<Point> {
      */
     public int compareTo(Point that) {
         /* YOUR CODE HERE */
-        return -1;
+        if (that == null)    throw new NullPointerException();
+
+        int dY = that.y - y;     // distance of direction Y
+        int dX = that.x - x;     // distance of direction X
+
+        if (dY < 0 || (dY == 0 && dX < 0))  return -1;
+        if (dY > 0 || (dY == 0 && dX > 0))  return 1;
+
+        return 0;
     }
 
     /**
@@ -88,7 +108,20 @@ public class Point implements Comparable<Point> {
      */
     public Comparator<Point> slopeOrder() {
         /* YOUR CODE HERE */
-        return null;
+        return new Comparator<Point>() {
+
+            @Override
+            public int compare(Point p1, Point p2) {
+                if (p1 == null || p2 == null)    throw new NullPointerException();
+
+                double slope1 = slopeTo(p1);
+                double slope2 = slopeTo(p2);
+
+                if (slope1 > slope2)     return 1;
+                if (slope1 < slope2)     return -1;
+                return 0;
+            }
+        };
     }
 
 
@@ -109,5 +142,44 @@ public class Point implements Comparable<Point> {
      */
     public static void main(String[] args) {
         /* YOUR CODE HERE */
+        Point[] points = new Point[4];
+        points[0] = new Point(0, 0);
+        points[1] = new Point(1, 1);
+        points[2] = new Point(0, 1);
+        points[3] = new Point(1, 0);
+
+        for (int i = 0; i < points.length; i++)
+        {
+            for (int j = 0; j < points.length; j++)
+            {
+//                printSlope(points[i], points[j]);
+                printCompare(points[i], points[j]); 
+                printOrder(points[0], points[i], points[j]);
+                System.out.println("--------------------------");
+            }
+        }
+    }
+
+    private static void printOrder(Point p0, Point p1, Point p2) 
+    {
+        printSlope(p0, p1);
+        printSlope(p0, p2);
+        double ans = p0.slopeOrder().compare(p1, p2);
+        if (ans > 0)        System.out.println("Slope from " + p0.toString() + " to " + 
+                p1.toString() + " is bigger than " + p2.toString() + "'s");   
+        else if (ans < 0)   System.out.println("Slope from " + p0.toString() + " to " + 
+                p1.toString() + " is smaller than " + p2.toString() + "'s");
+        else                System.out.println("Slope from " + p0.toString() + " to " + 
+                p1.toString() + " is identical to " + p2.toString() + "'s");
+    }
+    private static void printCompare(Point p0, Point p1)
+    {
+        if (p0.compareTo(p1) < 0)       System.out.println(p0.toString() + " is larger than " + p1.toString());
+        else if (p0.compareTo(p1) > 0)  System.out.println(p0.toString() + " is smaller than " + p1.toString());
+        else                            System.out.println("You input two identical points");
+    }
+    private static void printSlope(Point p0, Point p1) 
+    {
+        System.out.println("Slope of " + p0.toString() + "-->" + p1.toString() + " is " + p0.slopeTo(p1));
     }
 }
