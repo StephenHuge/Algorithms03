70213b2c1555251b8b128b32348b63cc51e4feca
HJS <407410014@qq.com>
2017-10-21 12:04:53 +0800

    add comments for BruteCollinearPoints.java

--- src/BruteCollinearPoints.java
+++ src/BruteCollinearPoints.java
@@ -1,4 +1,15 @@
 
+/**
+ * examines 4 points at a time and checks whether they all lie on the same line segment, 
+ * returning all such line segments. To check whether the 4 points p, q, r, and s are 
+ * collinear, check whether the three slopes between p and q, between p and r, and between
+ * p and s are all equal.
+ * 
+ * @author HJS
+ * 
+ * @date 2017-10-21
+ * 
+ */
 public class BruteCollinearPoints {
     public BruteCollinearPoints(Point[] points)    // finds all line segments containing 4 points
     {
