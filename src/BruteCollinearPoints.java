03b957d837d2b036922944e608599903683e7ad7
HJS <407410014@qq.com>
2017-10-23 23:29:02 +0800

    clean Brute..java and Fast..java

--- src/BruteCollinearPoints.java
+++ src/BruteCollinearPoints.java
@@ -13,162 +13,17 @@ import edu.princeton.cs.algs4.In;
  */
 public class BruteCollinearPoints {
 
-    private final Point[] points;
-    
-    private int length = 0;
-
     public BruteCollinearPoints(Point[] ps)    // finds all line segments containing 4 points
     {
-        validate(ps);
-        this.points = copy(ps);
-    }
-    private Point[] copy(Point[] ps) {
-        Point[] copy = new Point[ps.length];
-        for (int i = 0; i < ps.length; i++) {
-            copy[i] = ps[i];
-        }
-        return copy;
-    }
-    /**
-     * validate method, check whether array points is null, members of points are null
-     * or contain repeated points. If so, throw a java.lang.IllegalArgumentException
-     */
-    private void validate(Point[] ps) 
-    {
-        if (ps == null || (repeated(ps) < 0) || validateNullEntry(ps))
-            throw new java.lang.IllegalArgumentException();
-    }
-    private boolean validateNullEntry(Point[] ps) {
-        for (Point p : ps)
-            if (p == null)  return true;
-        return false;
-    }
-    /**
-     * check whether array points contains repeated points or members of points are null
-     */
-    private int repeated(Point[] ps) 
-    {
-        for (int i = 0; i < ps.length; i++) {
-            for (int j = i + 1; j < ps.length; j++) {
-                if (ps[i] == null || ps[j] == null
-                        || ps[i].compareTo(ps[j]) == 0)   return -1;
-            }
-        }
-        return 1;
     }
     public int numberOfSegments()        // the number of line segments
     {
-        if (length == 0)    length = segments().length;
-        return length;
+        return -1;
     }
     public LineSegment[] segments()                // the line segments
     {
-        int len = points.length * (points.length + 1) / 2;
-        LineSegment[] lineSegments = new LineSegment[len];
-        Line[] lines = new Line[len];
-        int pivot = 0;          // pivot for array lineSegments
-
-        for (int i = 0; i < points.length - 3; i++) {
-            double[] slopes = new double[3];
-            for (int j = i+ 1; j < points.length - 2; j++) {
-                slopes[0] = points[i].slopeTo(points[j]);           // first line
-                for (int k = j + 1; k < points.length - 1; k++) {
-                    slopes[1] = points[i].slopeTo(points[k]);       // second line
-                    for (int m = k + 1; m < points.length; m++) {
-                        slopes[2] = points[i].slopeTo(points[m]);   // third line
-                        
-                        if (collinear(slopes)) {        // three slope are collinear
-                            Line line = getMaxLine(points, i, j, k, m);   // get one max line
-                            int repeated = 0;
-                            repeated = line.notRepeated(lines);
-                            
-                            if (repeated == -1) {   // line not repeated, just add a new line
-                                lines[pivot] = line;
-                                lineSegments[pivot] = new LineSegment(line.min, line.max);
-                                pivot++;
-                            } else if (repeated != -2) { // line repeated, replace the old repeated one with a new one
-                                lines[repeated] = line;
-                                lineSegments[repeated] = new LineSegment(lines[repeated].min, lines[repeated].max);
-                            } 
-                        } 
-                    }
-                }
-            }
-        }
-        lineSegments = trim(lineSegments, pivot);    // trim array lineSegments with no null elements
-        return lineSegments;
+        return null;
     } 
-    private LineSegment[] trim(LineSegment[] lineSegments, int pivot) {
-        LineSegment[] newSegments = new LineSegment[pivot];
-        for (int i = 0; i < pivot; i++) {
-            newSegments[i] = lineSegments[i];
-        }
-        return newSegments;
-    }
-    private Line getMaxLine(Point[] points, int start, int end1, int end2, int end3) {
-        Point min = points[start];
-        Point max = points[start];
-        Point[] fourPoints = {points[start], points[end1], points[end2], points[end3]};
-        for (int i = 0; i < fourPoints.length; i++) {
-            if (fourPoints[i].compareTo(min) < 0)   min = fourPoints[i];
-            if (fourPoints[i].compareTo(max) > 0)   max = fourPoints[i];
-        }
-        return new Line(min, max);
-    }
-    private boolean collinear(double[] slopes) {
-        double start = slopes[0];
-        for (int i = 1; i < slopes.length; i++) {
-            if (Double.compare(start, slopes[i]) != 0)     return false;
-        }
-        return true;
-    }
-    private static class Line {
-        Point min;
-        Point max;
-        public Line(Point min, Point max) {
-            if (min.compareTo(max) > 0) {
-                Point temp = min;
-                max = min;
-                min = temp;
-            }
-            this.min = min;
-            this.max = max;
-        }
-        /**
-         * check whether there is repeating line in array lines, if so, return pivot {@code i} 
-         * of the array, else return {@code -1}.
-         */
-        public int notRepeated(Line[] lines) {
-            for (int i = 0; i < lines.length; i++) {
-                if (lines[i] == null)  break;
-                if (Double.compare(getSlope(), lines[i].getSlope()) == 0
-                        && hasSameEndPoint(lines[i])) {   // have same slope
-                    if (this.min.compareTo(lines[i].min) <= 0 
-                            && this.max.compareTo(lines[i].max) >= 0) {
-                        return i;
-                    }
-                    return -2;  // same slope but "smaller", just ignore this line
-                }
-            }
-            return -1;
-        }
-        public boolean hasSameEndPoint(Line line) {
-            if (this.min.compareTo(line.min) == 0 ||
-                    this.min.compareTo(line.max) == 0 ||
-                    this.max.compareTo(line.min) == 0 ||
-                    this.max.compareTo(line.max) == 0) {
-                return true;
-            }
-            return false;
-        }
-        public double getSlope() {
-            return max.slopeTo(min);
-        }
-        @Override
-        public String toString() {
-            return "Line " + min + " -> " + max;
-        }
-    }
     public static void main(String[] args) {
         In in = new In(args[0]);
         int n = in.readInt();
@@ -177,7 +32,6 @@ public class BruteCollinearPoints {
             int x = in.readInt();
             int y = in.readInt();
             points[i] = new Point(x, y);
-            //            System.out.println(points[i].toString());
         }
         BruteCollinearPoints bcp = new BruteCollinearPoints(points);
         LineSegment[] ls = bcp.segments();
--- src/FastCollinearPoints.java
+++ src/FastCollinearPoints.java
@@ -1,173 +1,17 @@
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.Merge;
 
 public class FastCollinearPoints {
 
-    private final Point[] points;
-
-    private int length = 0;
-
     public FastCollinearPoints(Point[] ps)     // finds all line segments containing 4 or more points
     {
-        validate(ps);
-        this.points = copy(ps);
-    }
-    private Point[] copy(Point[] ps) {
-        Point[] copy = new Point[ps.length];
-        for (int i = 0; i < ps.length; i++) {
-            copy[i] = ps[i];
-        }
-        return copy;
-    }
-    /**
-     * validate method, check whether array points is null, members of points are null
-     * or contains repeated points. If so, throw a java.lang.IllegalArgumentException
-     */
-    private void validate(Point[] ps) 
-    {
-        if (ps == null || (repeated(ps) < 0) || validateNullEntry(ps))
-            throw new java.lang.IllegalArgumentException();
-    }
-    private boolean validateNullEntry(Point[] ps) {
-        for (Point p : ps)
-            if (p == null)  return true;
-        return false;
-    }
-    /**
-     * check whether array points contains repeated points or members of points are null
-     */
-    private int repeated(Point[] ps) 
-    {
-        for (int i = 0; i < ps.length; i++) {
-            for (int j = i + 1; j < ps.length; j++) {
-                if (ps[i] == null || ps[j] == null
-                        || ps[i].compareTo(ps[j]) == 0)   return -1;
-            }
-        }
-        return 1;
     }
     public int numberOfSegments()                  // the number of line segments
     {
-        if (length == 0)    length = segments().length;
-        return length;
+       return -1;
     }
     public LineSegment[] segments()                // the line segments
     {
-        int len = points.length * (points.length + 1) / 2;
-        LineSegment[] lineSegments = new LineSegment[len];
-        Line[] max = new Line[len];
-        int index = 0;      // index for lineSegments 
-
-        for (int i = 0; i < points.length; i++) {
-            Line[] lines = new Line[points.length - 1];
-            {
-                int p = 0;
-                for (int j = 0; j < points.length; j++) {
-                    if (i != j)     lines[p++] = new Line(points[i], points[j]); 
-                }
-            }
-            Merge.sort(lines);      // sort array slopes
-
-//            System.out.println("------------");
-//            System.out.println("Lines: ");
-//            for (Line l : lines)
-//                System.out.println(l + " ");
-//            System.out.println("------------");
-
-            int pivotL = 0;      // pivot left to multiple array slopes
-            int pivotR = 1;      // pivot right to multiple array slopes
-            
-            while (pivotL < lines.length && pivotR < lines.length) {
-                while (pivotR < lines.length && 
-                        Double.compare(lines[pivotL].getSlope(), lines[pivotR].getSlope()) == 0)
-                    pivotR++;
-                if (pivotR - pivotL >= 3) {
-                    Line line = new Line(lines[pivotL].start, lines[pivotR - 1].end);
-                    if (notRepeated(max, line)) {
-                        max[index] = line;
-                        lineSegments[index] = new LineSegment(lines[pivotL].start, lines[pivotR - 1].end);
-                        index++;
-                        
-//                        System.out.println("Max Line: ");
-//                        System.out.println(l.start + " -> " + l.end);
-                    }
-                }
-                pivotL = pivotR;
-                pivotR = pivotR + 1;
-            }
-        }
-        lineSegments = trim(lineSegments, index);
-        return lineSegments;
-    }
-    private boolean notRepeated(Line[] max, Line line) {
-        if (max[0] == null) return true;
-        for (int i = 0; i < max.length; i++) {
-            if (max[i] == null)     break;
-            
-//            System.out.println("max " + max[i].start + " " + max[i].end);
-//            System.out.println("l " + l.start + " " + l.end);
-            
-            if (Double.compare(max[i].getSlope(), line.getSlope()) == 0 && 
-                    line.hasSameEndPoint(max[i])) {
-                if (line.start.compareTo(max[i].start) >= 0 &&
-                        line.end.compareTo(max[i].end) <= 0) {
-//                    System.out.println("repeated" + l.start + " -> " + l.end);
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-    private LineSegment[] trim(LineSegment[] lineSegments, int pivot) {
-        LineSegment[] newSegments = new LineSegment[pivot];
-        for (int i = 0; i < pivot; i++) {
-            newSegments[i] = lineSegments[i];
-        }
-        return newSegments;
-    }
-    private class Line implements Comparable<Line> {
-        Point start;
-        Point end;
-        public Line(Point p1, Point p2) {
-            if (p1.compareTo(p2) > 0) {
-                this.start = p2;
-                this.end = p1;
-            } else {
-                this.start = p1;
-                this.end = p2;
-            }
-        }
-
-        public boolean hasSameEndPoint(Line line) {
-            if (this.start.compareTo(line.start) == 0 ||
-                    this.start.compareTo(line.end) == 0 ||
-                    this.end.compareTo(line.start) == 0 ||
-                    this.end.compareTo(line.end) == 0) {
-                return true;
-            }
-            return false;
-        }
-
-        public double getSlope() {
-            return start.slopeTo(end);
-        }
-
-        @Override
-        public String toString() {
-            return "Line " + start + " -> " + end + " " + getSlope();
-        }
-
-        @Override
-        public int compareTo(Line line) {
-            int ans = Double.compare(this.getSlope(), line.getSlope());
-            if (ans < 0)  return -1;
-            if (ans > 0)  return 1;
-            else {
-                if (this.end.compareTo(line.end) < 0)   return -1;
-                if (this.end.compareTo(line.end) > 0)   return 1;
-                return 0;
-            }    
-        }
+        return null;
     }
     public static void main(String[] args) {
         In in = new In(args[0]);
