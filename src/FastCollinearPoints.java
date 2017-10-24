b804b3cce2e5713ff3350d4b189f8706993cef53
HJS <407410014@qq.com>
2017-10-24 15:49:10 +0800

    add Version2

--- src/FastCollinearPoints.java
+++ src/FastCollinearPoints.java
@@ -1,5 +1,5 @@
 import java.util.Arrays;
-
+import java.util.Comparator;
 
 import edu.princeton.cs.algs4.In;
 
@@ -18,8 +18,8 @@ public class FastCollinearPoints {
             points[i] = ps[i];
         }
         Arrays.sort(points);
-
-        lineSegments = new LineSegment[ps.length * (ps.length + 1) / 2];
+        lineSegments = getSegments();
+        length = lineSegments.length;
     }
     /**
      * validate method, check whether array points is null, members of points are null
@@ -38,60 +38,83 @@ public class FastCollinearPoints {
         }
     }
 
-    public int numberOfSegments()                  // the number of line segments
-    {
-        return length;
+    private LineSegment[] getSegments() {
+        Line[] lines = getLines(points);
+        return getSegmentsByLines(lines);
     }
-    public LineSegment[] segments()                // the line segments
-    {
+    
+    private Line[] getLines(Point[] ps) {
         Line[] lines = new Line[points.length * (points.length + 1) / 2];
         int index = 0;
 
         for (int i = 0; i < points.length - 3; i++) {
             Point origin = points[i];
+            Comparator<Point> sortOrder = origin.slopeOrder();
+
             Point[] mPoints = new Point[points.length - i - 1]; // copy to sort and store operation
             for (int j = i + 1; j < points.length; j++) {
                 mPoints[j - i - 1] = points[j];
             }
-            Arrays.sort(mPoints, origin.slopeOrder());  // sort with order slopeOrder
+            Arrays.sort(mPoints, sortOrder);  // sort with order origin.slopeOrder()
 
-            int left = 0;
+            int left = 0;       // pivots for getting maximum lines 
             int right = 1;
-
             while (left < mPoints.length && right < mPoints.length) {
-//                System.out.println("origin -> mPoints[left] " + 
-//                                origin.toString() + " -> " + mPoints[left].toString() + 
-//                                ", slope: " +origin.slopeTo(mPoints[left])+ 
-//                                " \norigin -> mPoints[right] " + 
-//                                origin.toString() +" ->  " + mPoints[right].toString() + 
-//                                ", slope: " + origin.slopeTo(mPoints[right]) +
-//                                "\n------------------------------");
                 while (right < mPoints.length &&  
-                        origin.slopeOrder().
-                        compare(mPoints[left], mPoints[right]) == 0) {
+                        sortOrder.compare(mPoints[left], mPoints[right]) == 0) {    // same slope
                     right++;
                 } 
-                if (right - left >= 3) {
+                if (right - left >= 3) {            // more than 3 points, add new line
                     lines[index++] = new Line(origin, mPoints[right - 1]);
-                    System.out.println(String.format("new Line : %s", lines[index - 1].toString()));
+//                    System.out.println(String.format("new Line : %s", lines[index - 1].toString()));
                 }
                 left = right;
                 right = right + 1;
             }
         }
-//        lines = trim(lines);
-        return getSegmentsByLines(lines, index);
-    }
-    private Line[] trim(Line[] lines) {
-        return null;
-    }
-    private LineSegment[] getSegmentsByLines(Line[] lines, int index) {
-        LineSegment[] lineSegments = new LineSegment[index];
+        Line[] newLines = new Line[index];
         for (int i = 0; i < index; i++) {
+            newLines[i] = lines[i];
+        }
+        lines = trim(newLines, index);      // there may be some repeated lines here
+        
+        return lines;
+    }
+    /**
+     * after sort, lines got larger slope or smaller min point when same slope is latter
+     */
+    private Line[] trim(Line[] lines, int index) {
+        Arrays.sort(lines);
+        
+//        System.out.println("-----------sorted Lines---------------");
+//        for (Line l : lines)
+//            System.out.println(l.toString());
+//        System.out.println("-----------sorted Lines---------------");
+        int left = 0;
+        int right = 1;
+        int pivot = 0;
+        while (left < lines.length && right < lines.length) {
+            while (right < lines.length  && lines[left].getMax().compareTo(lines[right].getMax()) == 0 && 
+                    Double.compare(lines[left].getSlope(), lines[right].getSlope()) == 0) {
+                right++;
+            }
+            lines[pivot++] = lines[right - 1];
+            left = right;
+//            right = right + 1;
+        }
+        Line[] newLine = new Line[pivot];
+        for (int i = 0; i < pivot; i++)
+            newLine[i] = lines[i];
+        return newLine;
+    }
+    private LineSegment[] getSegmentsByLines(Line[] lines) {
+        LineSegment[] lineSegments = new LineSegment[lines.length];
+        for (int i = 0; i < lines.length; i++) {
             lineSegments[i] = new LineSegment(lines[i].getMin(), lines[i].getMax());
         }
         return lineSegments;
     }
+    
     private class Line implements Comparable<Line>{
         private Point min;
         private Point max;
@@ -104,14 +127,8 @@ public class FastCollinearPoints {
         public Point getMin() {
             return min;
         }
-        public void setMin(Point min) {
-            this.min = min;
-        }
         public Point getMax() {
             return max;
-        }
-        public void setMax(Point max) {
-            this.max = max;
         }
         public double getSlope() {
             slope = min.slopeTo(max);
@@ -126,16 +143,33 @@ public class FastCollinearPoints {
             if (Double.compare(getSlope(), o.getSlope()) < 0)    return -1;
             if (Double.compare(getSlope(), o.getSlope()) > 0)    return 1;
             else {
-                if (getMin().compareTo(o.getMin()) < 0)     // this line has smaller min point than o, so it's **bigger**
+                if (getMax().compareTo(o.getMax()) > 0)
                     return 1;
-                else if (getMin().compareTo(o.getMin()) > 0) // this line has bigger min point than o, so it's **smaller**
+                else if (getMax().compareTo(o.getMax()) < 0)
                     return -1;
-                else
-                    return 0;
+                else {
+                    if (getMin().compareTo(o.getMin()) < 0)     // this line has smaller min point than o, so it's **bigger**
+                        return 1;
+                    else if (getMin().compareTo(o.getMin()) > 0) // this line has bigger min point than o, so it's **smaller**
+                        return -1;
+                    else
+                        return 0;
+                }
             }
         }
     }
-
+    public int numberOfSegments()                  // the number of line segments
+    {
+        return length;
+    }
+    public LineSegment[] segments()                // the line segments
+    {
+        LineSegment[] newSegments = new LineSegment[lineSegments.length];
+        for (int i = 0; i < lineSegments.length; i++) {
+            newSegments[i] = lineSegments[i];
+        }
+        return newSegments;
+    }
     public static void main(String[] args) {
         In in = new In(args[0]);
         int n = in.readInt();
